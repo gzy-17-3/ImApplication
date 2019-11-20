030ea9ece847032ebf7656532ca9b387731a03dc
lxf <rdxer@foxmail.com>
2019-11-20 21:58:41 +0800

    Fragment 的切换完成

--- app/src/main/java/com/gzy/imapplication/module/home/HomeActivity.java
+++ app/src/main/java/com/gzy/imapplication/module/home/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.gzy.imapplication.module.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,13 +12,22 @@ import com.gzy.imapplication.R;
 import com.gzy.imapplication.core.Auth;
 import com.gzy.imapplication.module.auth.LoginActivity;
 import com.gzy.imapplication.module.base.BaseActivity;
+import com.gzy.imapplication.module.contacts.ContactsFragment;
+import com.gzy.imapplication.module.message.MessageFragment;
+import com.gzy.imapplication.module.mine.MineFragment;
 
 public class HomeActivity extends BaseActivity {
+
+    Fragment contactsFragment;
+    Fragment messageFragment;
+    Fragment mineFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        initFragment(R.id.item_tabbar_message);
     }
 
     public void onClickLogout(View view) {
@@ -34,10 +45,34 @@ public class HomeActivity extends BaseActivity {
     }
 
 
+
+    private void initFragment(int startShow) {
+
+        /*获取manager*/
+        FragmentManager manager = this.getSupportFragmentManager();
+
+        /*通过findFragmentById获取Fragment*/
+        contactsFragment =  manager.findFragmentById(R.id.f_ContactsFragment);
+        messageFragment =  manager.findFragmentById(R.id.f_MessageFragment);
+        mineFragment =  manager.findFragmentById(R.id.f_MineFragment);
+
+        // 默认隐藏所有的
+        contactsFragment.getView().setVisibility(View.GONE);
+        messageFragment.getView().setVisibility(View.GONE);
+        mineFragment.getView().setVisibility(View.GONE);
+
+        View view = findViewById(startShow);
+        onClickTabbar(view);
+    }
+
+
     View currentTabbarItemView;
+    Fragment currentFragment;
+
 
     public void onClickTabbar(View view) {
 
+        //  view  的 切换
         if (currentTabbarItemView != null) {
             currentTabbarItemView.setSelected(false);
         }
@@ -46,15 +81,25 @@ public class HomeActivity extends BaseActivity {
 
         currentTabbarItemView = view;
 
+
+        //  Fragment  的 切换
+        if (currentFragment != null) {
+            currentFragment.getView().setVisibility(View.GONE);
+        }
+
+
         switch (view.getId() ){
             case R.id.item_tabbar_contacts:
-
+                contactsFragment.getView().setVisibility(View.VISIBLE);
+                currentFragment = contactsFragment;
                 break;
             case R.id.item_tabbar_message:
-
+                messageFragment.getView().setVisibility(View.VISIBLE);
+                currentFragment = messageFragment;
                 break;
             case R.id.item_tabbar_mine:
-
+                mineFragment.getView().setVisibility(View.VISIBLE);
+                currentFragment = mineFragment;
                 break;
         }
 
