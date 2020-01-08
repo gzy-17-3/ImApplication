aed734d24d58b05b267d7d9865a2a385081b6230
lxf <rdxer@foxmail.com>
2020-01-08 08:43:27 +0800

    优化列表页

--- app/src/main/java/com/gzy/imapplication/module/message/MessageAdapter.java
+++ app/src/main/java/com/gzy/imapplication/module/message/MessageAdapter.java
@@ -10,6 +10,7 @@ import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.gzy.imapplication.R;
 import com.gzy.imapplication.model.Account;
+import com.gzy.imapplication.model.Chat;
 import com.gzy.imapplication.model.ChatSession;
 
 import java.util.ArrayList;
@@ -19,12 +20,20 @@ class MessageAdapter  extends BaseQuickAdapter<ChatSession, BaseViewHolder> {
 
 
     public MessageAdapter(@Nullable List<ChatSession> data) {
-        super(R.layout.item_student,data);
+        super(R.layout.item_chat_session,data);
     }
 
     @Override
     protected void convert(@NonNull BaseViewHolder helper, ChatSession item) {
         helper.setText(R.id.tv_name,item.getAccount().getName());
+
+        Chat lastChat = item.getLastChat();
+
+        if (lastChat != null) {
+            helper.setText(R.id.tv_desc,lastChat.getContent());
+        }else{
+            helper.setText(R.id.tv_desc,"无");
+        }
 
         Glide.with(mContext)
                 .load(item.getAccount().getAvatarUrlString())
